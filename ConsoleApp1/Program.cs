a3f477c628804b4a9c36edb2e46959cdbc02654f
frankshicar <frankshicar@gmail.com>
2024-01-29 18:05:21 +0800

    try print src ip port and dst ip port

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Xml.Linq;
+using System.Net;
+
 namespace ConsoleApp1
 {
     class Program
@@ -27,7 +29,7 @@ namespace ConsoleApp1
 
             // 讀取 XML 配置文件
             //var config = XDocument.Load("etwrole.xml");
-            var config = XDocument.Load(@"C:\Users\User\Desktop\etw test\etw\ConsoleApp1\bin\Debug\etwrole.xml");
+            var config = XDocument.Load(@"C:\Users\User\Desktop\etw test\etw\etwrole.xml");
             var watcherConfig = config.Element("Configuration").Element("FileSystemWatcherConfig");
             var processMonitorConfig = config.Element("Configuration").Element("ProcessMonitorConfig");
 
@@ -94,25 +96,51 @@ namespace ConsoleApp1
             //};
 
             // 處理網絡事件
+            //traceEventSession.Source.Dynamic.All += data =>
+            //{
+            //    if (data.ProviderName == "Microsoft-Windows-TCPIP")
+            //    {
+            //        // 检查是否是 TCP 连接事件
+            //        if (data.EventName == "TcpConnectTcbComplete" || data.EventName == "TcpDisconnect")
+            //        {
+            //            var localAddressBytes = data.PayloadByName("LocalAddress") as byte[];
+            //            var remoteAddressBytes = data.PayloadByName("RemoteAddress") as byte[];
+            //            var localPort = data.PayloadByName("LocalPort");
+            //            var remotePort = data.PayloadByName("RemotePort");
+
+            //            var localAddress = localAddressBytes != null ? BytesToIPAddressString(localAddressBytes) : "Unavailable";
+            //            var remoteAddress = remoteAddressBytes != null ? BytesToIPAddressString(remoteAddressBytes) : "Unavailable";
+
+            //            Console.WriteLine($"[TCP Event] {data.EventName} at {data.TimeStamp}");
+            //            Console.WriteLine($"  Local Address: {localAddress}, Local Port: {localPort}");
+            //            Console.WriteLine($"  Remote Address: {remoteAddress}, Remote Port: {remotePort}");
+            //        }
+            //    }
+            //};
+
             traceEventSession.Source.Dynamic.All += data =>
             {
                 if (data.ProviderName == "Microsoft-Windows-TCPIP")
                 {
-                    if (data.EventName == "TcpConnectTcbComplete")
+                    // 检查是否是 TCP 连接事件
+                    if (data.EventName == "TcpConnectTcbComplete" || data.EventName == "TcpConnectAttempt")
                     {
-                        Console.WriteLine($"[TcpConnectTcbCompleteEvent] at {data.TimeStamp}");
-                        foreach (var payloadName in data.PayloadNames)
-                        {
-                            var payloadValue = data.PayloadByName(payloadName);
-                            if (payloadName == "LocalAddress" || payloadName == "RemoteAddress")
-                            {
-                                payloadValue = BytesToIPAddressString(payloadValue as byte[]);
-                            }
-                            Console.WriteLine($"  {payloadName}: {payloadValue}");
-                        }
+                        var localAddressBytes = data.PayloadByName("LocalAddress") as byte[];
+                        var remoteAddressBytes = data.PayloadByName("RemoteAddress") as byte[];
+                        var localPort = data.PayloadByName("LocalPort");
+                        var remotePort = data.PayloadByName("RemotePort");
+
+                        var localAddress = localAddressBytes != null ? BytesToIPAddressString(localAddressBytes) : "Unavailable";
+                        var remoteAddress = remoteAddressBytes != null ? BytesToIPAddressString(remoteAddressBytes) : "Unavailable";
+
+                        Console.WriteLine($"[TCP Connect Event] {data.EventName} at {data.TimeStamp}");
+                        Console.WriteLine($"  Source IP: {localAddress}, Source Port: {localPort}");
+                        Console.WriteLine($"  Destination IP: {remoteAddress}, Destination Port: {remotePort}");
                     }
                 }
             };
+
+
 
             var etwThread = new Thread(() => traceEventSession.Source.Process());
             etwThread.Start();
@@ -120,12 +148,42 @@ namespace ConsoleApp1
 
         private static string BytesToIPAddressString(byte[] bytes)
         {
-            if (bytes == null || bytes.Length != 4 && bytes.Length != 16)
+            if (bytes == null)
+            {
+                return "Unavailable";
+            }
+
+            try
+            {
+                IPAddress ip = new IPAddress(bytes);
+                return ip.ToString();
+            }
+            catch
             {
                 return "Invalid IP Address";
             }
-            return new IPAddress(bytes).ToString();
         }
+
+
+        //private static string BytesToIPAddressString(byte[] bytes)
+        //{
+        //    if (bytes == null)
+        //    {
+        //        return "Invalid IP Address: Null bytes array";
+        //    }
+
+        //    try
+        //    {
+        //        IPAddress ip = new IPAddress(bytes);
+        //        return ip.ToString();
+        //    }
+        //    catch (ArgumentException)
+        //    {
+        //        return $"Invalid IP Address: Byte array length {bytes.Length}";
+        //    }
+        //}
+
+
 
         static void InitializeFileSystemWatcher(string path, string filter, string notifyFilter)
         {
